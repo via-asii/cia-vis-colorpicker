e6e9c55e62207bf64e2a26515f44f41e187bea0b
David Lorente <david.l.castilla@gmail.com>
2019-02-08 12:03:04 -0500

    add toastr

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import {
 import {color, hex} from 'd3-color'
 import { perc2color } from './customScales';
 import './App.css';
+import toastr from 'toastr';
+import './Toastr.css';
 
 
 class App extends Component {
@@ -31,6 +33,7 @@ class App extends Component {
     el.select();
     document.execCommand('copy');
     document.body.removeChild(el);
+    toastr.success('Copied to clipboard', "" ,{timeOut: 1500})
   }
   render() {
     const cat10Scale = scaleOrdinal(schemeCategory10);
@@ -55,12 +58,12 @@ class App extends Component {
     for (var i = 1; i <= this.state.nColors; i++) {
       iterArray.push(i)
     }
-    console.log(color.hex);
+
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            Vis Color Picker
+            CIA Vis Color Generator
           </p>
         </header>
         <main>
@@ -104,17 +107,17 @@ class App extends Component {
 
               <div className="flex f-center scale">
                 {iterArray.map((i) => <div style={{height: '70px', width: '100px', backgroundColor: prgnScale(i)}}>
-                  <p onClick={()=>{this.copyToClipboard(prgnScale(i))}} >{color(prgnScale(i)).hex()}</p>
+                  <p onClick={()=>{this.copyToClipboard(color(prgnScale(i)).hex())}} >{color(prgnScale(i)).hex()}</p>
                 </div>)}
               </div>
 
               <div className="">
                 <p>Pick your own colors:</p>
-                <div className="flex f-center">
+                <div className="scale flex f-center">
                   <input type="color" id="html5colorpicker" onChange={(e)=> {this.setState({diyScale0: e.target.value}); } } value={this.state.diyScale0} />
                     <div className="flex">
                       {iterArray.map((i) => <div style={{height: '70px', width: '100px', backgroundColor: diyColor(i)}}>
-                        <p onClick={()=>{this.copyToClipboard(diyColor(i))}} >{color(diyColor(i)).hex()}</p>
+                        <p onClick={()=>{this.copyToClipboard(color(diyColor(i)).hex())}} >{color(diyColor(i)).hex()}</p>
                       </div>)}
                     </div>
                   <input type="color" id="html5colorpicker" onChange={(e)=> {this.setState({diyScale1: e.target.value}); } } value={this.state.diyScale1} />
